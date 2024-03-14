7fa7fc2c0fe7675cfa2bc16c3a66df65f4e35ff1
Fabian Bergermann <f.bergermann@hellogetsafe.com>
2024-03-14 18:32:20 +0100

    tests: update tests

---
+++ {{cookiecutter.__directory_name}}/tests/data/test_make_dataset.py
@@ -0,0 +1,13 @@
+from click.testing import CliRunner
+
+from {{ cookiecutter.__package_name }}.data.make_dataset import main, main_cli
+
+
+def test_main_cli():
+    runner = CliRunner()
+    result = runner.invoke(main_cli, ["--environment", "production"])
+    assert result.exit_code == 0
+
+
+def test_main():
+    assert main(environment="production") is None
--- {{cookiecutter.__directory_name}}/tests/test_train_model.py
+++
@@ -1,6 +0,0 @@
-from {{ cookiecutter.__package_name }}.train_model import train_model
-
-
-def test_train_model():
-    res = train_model()
-    assert res is None
